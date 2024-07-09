a1ad52b4ad77eebc7dab0fbaf6d50430be068af9
Omar Maldonado <omaldonado@apexsystems.com>
2024-07-08 18:42:32 -0600

    se ajustan campos forma inscripcion

--- js/inscripcion.js
+++ js/inscripcion.js
@@ -3,17 +3,54 @@ import { createApp, ref } from "vue"
 createApp({
   data() {
     return {
-      nombre: "",
-      apellidoPaterno: "",
-      apellidoMaterno: "",
-      dtNacimiento: "",
-      tipoCurso: 1,
-      tipoModalidad: 1,
+      alumno: {
+        nombre: "",
+        apellidoPaterno: "",
+        apellidoMaterno: "",
+        dtNacimiento: "",
+        iCurso: 1,
+        iModalidad: 1,
+        telefono: "",
+        email: "",
+        direccion: {
+          calle: "",
+          numero: "",
+          colonia: "",
+          cp: "",
+          ciudad: "",
+          iEstado: 1,
+        },
+      },
+      tutor: {
+        nombre: "",
+        iParentesco: 1,
+        telCelular: "",
+        telCasa: "",
+        email: "",
+        padecimientos: "",
+        notas: "",
+      },
+      contactoEmergencia: {
+        nombre: "",
+        parentesco: "",
+        telCelular: "",
+        telCasa: "",
+      },
     }
+  },
+  watch: {
+    "alumno.iModalidad"() {
+      this.contactoEmergencia = {
+        nombre: "",
+        parentesco: "",
+        telCelular: "",
+        telCasa: "",
+      }
+    },
   },
   computed: {
     esMayorEdad() {
-      const dtNacimiento = new Date(this.dtNacimiento)
+      const dtNacimiento = new Date(this.alumno.dtNacimiento)
       if (isNaN(dtNacimiento)) return false
 
       const dtActual = new Date()
@@ -47,16 +84,45 @@ createApp({
       return true
     },
     showContactoEmergencia() {
-      return this.tipoModalidad == 1
+      return this.alumno.iModalidad == 1
     },
   },
   methods: {
     enviarForma() {
       console.log({
-        nombre: this.nombre,
-        apellidoPaterno: this.apellidoPaterno,
-        apellidoMaterno: this.apellidoMaterno,
-        dtNacimiento: this.dtNacimiento,
+        alumno: {
+          nombre: this.alumno.nombre,
+          apellidoPaterno: this.alumno.apellidoPaterno,
+          apellidoMaterno: this.alumno.apellidoMaterno,
+          dtNacimiento: this.alumno.dtNacimiento,
+          iCurso: this.alumno.iCurso,
+          iModalidad: this.alumno.iModalidad,
+          telefono: this.alumno.telefono,
+          email: this.alumno.email,
+          direccion: {
+            calle: this.alumno.direccion.calle,
+            numero: this.alumno.direccion.numero,
+            colonia: this.alumno.direccion.colonia,
+            cp: this.alumno.direccion.cp,
+            ciudad: this.alumno.direccion.ciudad,
+            iEstado: this.alumno.direccion.iEstado,
+          },
+        },
+        tutor: {
+          nombre: this.tutor.nombre,
+          iParentesco: this.tutor.iParentesco,
+          telCelular: this.tutor.telCelular,
+          telCasa: this.tutor.telCasa,
+          email: this.tutor.email,
+          padecimientos: this.tutor.padecimientos,
+          notas: this.tutor.notas,
+        },
+        contactoEmergencia: {
+          nombre: this.contactoEmergencia.nombre,
+          parentesco: this.contactoEmergencia.parentesco,
+          telCelular: this.contactoEmergencia.telCelular,
+          telCasa: this.contactoEmergencia.telCasa,
+        },
       })
     },
   },
